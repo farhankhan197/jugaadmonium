ada48222eeae825b43fce7875b46bd7cfc7ebf8e
farhankhan197 <farhankhan.code@gmail.com>
2026-03-23 20:24:35 +0530

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Ergosphere",
-  description: "Tilt to play",
+  description: "tedha karo",
 };
 
 export const viewport: Viewport = {
